cf40a0b4749f4dc2ccfd731d22ac9fad60bb9e98
Marina Pavlova <marinap.learning@gmail.com>
2022-02-04 19:24:52 +0000

    Complete tasks for lesson 2

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 'use strict';
 
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let num = 266219;
 
-console.log("Надпись в консоле");
+//multiply all numbers in num
+let newNum = num.toString();
+let multply = newNum[0];
+for (let i = 0; i < newNum.length-1; i++) {
+    multply *= newNum[i+1];
+}
+console.log(multply);
 
-alert("Нажми на Ок");
+multply = multply**3;
+//show in console first 2 numbers of multply
+console.log(Math.trunc(multply / (10**8)));
