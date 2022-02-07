32c30684ff6e1815c6b4a90b0d9315fecc70633f
Marina Pavlova <marinap.learning@gmail.com>
2022-02-07 13:59:58 +0000

    Start of lesson 04

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//lesson 2
+console.log("\n lesson 2 ------------\n");
 let num = 266219;
 
 //multiply all numbers in num
@@ -13,3 +15,48 @@ console.log(multply);
 multply = multply**3;
 //show in console first 2 numbers of multply
 console.log(Math.trunc(multply / (10**8)));
+
+//lesson 3
+
+console.log("\n lesson 3 ------------\n");
+
+let lang = "en";
+
+//через if
+if (lang == "ru") {
+    console.log("понедельник");
+    console.log("вторник");
+    console.log("среда");
+    console.log("четверг");
+    console.log("пятница");
+    console.log("суббота");
+    console.log("воскресенье");
+} else if (lang == "en") {
+    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+} else {
+    console.log("Я не говорю на этом языке");
+}
+
+
+//через switch
+switch (lang) {
+    case "ru":
+        console.log("понедельник, вторник, среда, четверг, пятница, суббота и воскресенье");
+        break;
+    case "en":
+        console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
+        break;
+    default:
+        console.log("Я не говорю на этом языке");
+}
+
+// через многомерный массив без ифов и switch
+
+let langArr = [["понедельник, вторник, среда, четверг, пятница, суббота и воскресенье"], ["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"]];
+
+lang == "ru" ? console.log(langArr[0]) : lang == "en" ? console.log(langArr[1]) : console.log("Я не говорю на этом языке");
+
+//lesson 4
+
+console.log("\n lesson 4 ------------\n");
+
